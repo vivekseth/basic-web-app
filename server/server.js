a0dacd27e66edd3a823d4bb3af0ebc0e16aab543
Vivek Seth <Vivekseth.m@gmail.com>
2017-09-23 12:34:48 -0700

    Update server and organize

--- server/server.js
+++ server/server.js
@@ -5,12 +5,23 @@ const express = require('express');
 
 const app = express();
 
+// Static File Middleware 
+
 app.use('/static', express.static(path.join(__dirname, '../dist')));
+
+// API Methods
+
+app.get('/hello', function(req, res) {
+    res.send('hello\n');
+})
+
+// Catch-All Route
 
 app.get('*', function (req, res) {
     res.sendFile('app.html', {root: './dist'});
 });
 
+PORT = 3000;
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000!\n');
+    console.log('Example app listening on port '+PORT+'!\n');
 });
