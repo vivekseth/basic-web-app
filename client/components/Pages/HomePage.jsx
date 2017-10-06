128ecbe3a90232facd06ec96d1f52e74174ff6ce
Vivek Seth <Vivekseth.m@gmail.com>
2017-10-06 00:11:19 -0700

    Finally created AsynData component.

--- client/components/Pages/HomePage.jsx
+++ client/components/Pages/HomePage.jsx
@@ -1,53 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import superagent from 'superagent'
+import AsyncData from '../Util/AsyncData.jsx'
+
+// const _WelcomeMessage = (props) => {
+//   return (
+//     <h1>
+//       Hello {props.data.username} !
+//     </h1>
+//   );
+// }
 
 class HomePage extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      isLoading: false,
-      data: null
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      isLoading: true,
-    })
-
-    superagent
-      .get('http://localhost:8080/api/user/')
-      .end((err, res) => {
-        this.setState({
-          isLoading: false,
-          data: res.body.data
-        })
-      });
-  }
-
+  
   render() {
-    const _createContent = () => {
-      if (this.state.isLoading) {
-        return (<h1>loading...</h1>);
-      }
-      else if (!this.state.data) {
-        return (<h1>Log In for extra features!</h1>); 
-      }
-      else {
-        return (
-          <h1>
-            Hello {this.state.data.username} !
-          </h1>
-        ); 
-      }
-    }
-
-    return (
-      <div>
-        {_createContent()}
-      </div>
-    )
+    return <AsyncData 
+      apiURL='http://localhost:8080/api/user/'
+      IsLoading={(props) => {return (<h1>Loading...</h1>)}}
+      NoData={(props) => {return (<h1>Log In for extra features!</h1>)}}
+      HasData={(props) => {return (<h1>Hello {props.data.username} !</h1>)}}
+    />
   }
 }
 
---
+++ client/components/Util/AsyncData.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import superagent from 'superagent'
+
+class AsyncData extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: false,
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true,
+    })
+
+    superagent
+      .get(this.props.apiURL)
+      .end((err, res) => {
+        this.setState({
+          isLoading: false,
+          data: res.body.data
+        })
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <this.props.IsLoading />
+    }
+    else if (!this.state.data) {
+      return <this.props.NoData />
+    }
+    else {
+      return <this.props.HasData data={this.state.data} />
+    }
+  }
+}
+
+export default AsyncData;
