5676e37ea986303a22ddcacf20a8788ca8e9de7f
Vivek Seth <Vivekseth.m@gmail.com>
2017-10-06 00:01:26 -0700

    Basic login support

--- client/components/Pages/HomePage.jsx
+++ client/components/Pages/HomePage.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import superagent from 'superagent'
 
 class HomePage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: false,
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true,
+    })
+
+    superagent
+      .get('http://localhost:8080/api/user/')
+      .end((err, res) => {
+        this.setState({
+          isLoading: false,
+          data: res.body.data
+        })
+      });
+  }
+
   render() {
+    const _createContent = () => {
+      if (this.state.isLoading) {
+        return (<h1>loading...</h1>);
+      }
+      else if (!this.state.data) {
+        return (<h1>Log In for extra features!</h1>); 
+      }
+      else {
+        return (
+          <h1>
+            Hello {this.state.data.username} !
+          </h1>
+        ); 
+      }
+    }
+
     return (
       <div>
-        <h1>HomePage</h1>
+        {_createContent()}
       </div>
     )
   }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const request = require('request');
 const express = require('express');
 const jsonfile = require('jsonfile');
 
+const session = require("express-session");
+const bodyparser = require('body-parser');
+
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+const ensureLoggedIn = require('connect-ensure-login');
+
 const PRODUCTION = !!(process.env['PRODUCTION'])
 const PORT = process.env.PORT || 3000;
 let CACHE_DIRTY = false;
@@ -16,19 +23,106 @@ setInterval(function() {
   }
 }, 1000)
 
-
-
 const app = express();
 
-// Static File Middleware 
+// User Authentication
 
+var userDB = {'hello': {
+  'password': 'world'
+}}
+
+const _validateCredentials = (username, password) => {
+  if (userDB[username]) {
+    const userData = userDB[username];
+    return userData['password'] === password;
+  }
+  else {
+    return false;
+  }
+}
+
+const _registerUser = (username, password) => {
+  if (!userDB[username]) {
+    userDB[username] = {
+      'password': password,
+    }
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (_validateCredentials(username, password)) {
+      done(null, {username: username});
+    }
+    else {
+      done(null, false);
+    }
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(userid, done) {
+  done(null, {username: userid});
+});
+
+
+// serve static files from ../dist
 app.use(express.static(path.join(__dirname, '../dist')));
+
+// parse form http requests
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// user auth middleware
+app.use(session({
+  secret: 'sooper seecrit seshun kee',
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // API Methods
 
 // app.get('/api/hello', function(req, res) {
 //     res.json({hello: "world"});
 // });
+
+app.get('/api/user', function(req, res) {
+  if (req.user) {
+    res.json({
+      success: true,
+      data: req.user,
+    });
+  }
+  else {
+    res.json({
+      success: false,
+      data: null,
+    });
+  }
+});
+
+app.get('/api/user/favorites', function(req, res) {
+  if (req.user) {
+    res.json({
+      success: true,
+      data: [],
+    });
+  }
+  else {
+    res.json({
+      success: false,
+      data: null,
+    });
+  }
+})
 
 app.get('/api/*', function(req, res) {
   const apiURL = 'https://swapi.co' + req.url;
@@ -54,19 +148,41 @@ app.get('/api/*', function(req, res) {
   }
 })
 
-// User Management
+const loginPath = PRODUCTION ? '/login' : '/#/login'
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: loginPath }));
 
-app.post('/login', function(req, res) {
-  console.log('LOGIN');
-  res.send('<script>window.location = "/"; </script>');
-});
+// app.post('/login', function(req, res) {
+//   console.log('LOGIN');
+//   console.log(req.body);
+
+//   if (_validateCredentials(req.body.username, req.body.password)) {
+//     res.send('valid credentials!');
+//   }
+//   else {
+//     res.send('INVALID credentials!'); 
+//   }
+// });
 
 app.post('/register', function(req, res) {
   console.log('REGISTER');
-  res.send('<script>window.location = "/"; </script>');
+  console.log(req.body);
+
+  if (_registerUser(req.body.username, req.body.password)) {
+    res.send('registration successful!');
+  }
+  else {
+    res.send('error, user already exists');
+  }
 });
 
 // Catch-All Route
+
+app.post('/', function (req, res) {
+    res.sendFile('index.html', {root: './dist'});
+});
+
 
 if (PRODUCTION) {
     app.get('*', function (req, res) {
