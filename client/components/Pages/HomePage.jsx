7021dee7977b07782626fbe57052b607783bf3a8
Vivek Seth <Vivekseth.m@gmail.com>
2017-10-07 12:56:07 -0700

    Refactor Home and AsyncFilmItem.jsx

--- client/components/Pages/HomePage.jsx
+++ client/components/Pages/HomePage.jsx
@@ -3,32 +3,27 @@ import { Link } from 'react-router-dom'
 import superagent from 'superagent'
 import AsyncData from '../Util/AsyncData.jsx'
 
-// const _WelcomeMessage = (props) => {
-//   return (
-//     <h1>
-//       Hello {props.data.username} !
-//     </h1>
-//   );
-// }
 
-class HomePage extends React.Component {
-  render() {
-    const NoData = (props) => {return (<h1>Log In for extra features!</h1>)};
 
-    return <AsyncData 
-      apiURL='http://localhost:8080/api/user/'
-      IsLoading={(props) => {return (<h1>Loading...</h1>)}}
-      NoData={NoData}
-      HasData={(props) => {
-        if (props.data.success) {
-          return (<h1>Hello {props.data.data.username} !</h1>)
-        }
-        else {
-          return NoData(props);
-        }
-      }}
-    />
-  }
+
+const NoData = (props) => {
+  return (<h1>Log In for extra features!</h1>)
+};
+
+const HomePage = (props) => {
+  return <AsyncData
+    apiURL='http://localhost:8080/api/user/'
+    IsLoading={(props) => {return (<h1>Loading...</h1>)}}
+    NoData={NoData}
+    HasData={(props) => {
+      if (props.data.success) {
+        return (<h1>Hello {props.data.data.username} !</h1>)
+      }
+      else {
+        return NoData(props);
+      }
+    }}
+  />
 }
 
 
--- client/components/Util/AsyncFilmItem.jsx
+++ client/components/Util/AsyncFilmItem.jsx
@@ -1,53 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import superagent from 'superagent'
+import AsyncData from './AsyncData.jsx'
 
-// TODO(vivek): Create higher-order component to present data that is loaded via API. 
+const AsyncFilmItem = (props) => {
+  const filmID = props.filmID;
 
-class AsyncFilmItem extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      isLoading: false,
-      data: null
+  return <AsyncData 
+    apiURL={
+      'http://localhost:8080/api/films/' + filmID
     }
-  }
-
-  _filmID() {
-    return this.props.filmID;
-  }
-
-  componentDidMount() {
-    this.setState({
-      isLoading: true,
-    })
-
-    superagent
-      .get('http://localhost:8080/api/films/' + this._filmID())
-      .end((err, res) => {
-        this.setState({
-          isLoading: false,
-          data: res.body
-        })
-      });
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return (<p>loading...</p>);
-    }
-    else if (!this.state.data) {
-      return (<p>There is no data with that ID</p>); 
-    }
-    else {
+    IsLoading={(props) => {
+      return (<p>Loading...</p>)
+    }}
+    NoData={(props) => {
+      return (<p>There is no data with that ID</p>)
+    }}
+    HasData={(props) => {
       return (
-        <Link to={'/films/' + this._filmID()}>
-          {this.state.data.title}
+        <Link to={'/films/' + filmID}>
+          {props.data.title}
         </Link>
       )
-    }
-  }
+    }}
+  />
 }
-
 
 export default AsyncFilmItem;
