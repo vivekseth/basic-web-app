f93f3f259f0b1eb59ab55d3f9325822102ee173f
Vivek Seth <Vivekseth.m@gmail.com>
2017-10-08 11:53:17 -0700

    Add bcrypt dependency + add default user (vivek, seth)

--- init.sql
+++ init.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
    username TEXT PRIMARY KEY NOT NULL,
    hash TEXT NOT NULL
 );
+INSERT INTO "users" VALUES('vivek','$2a$10$cEpt5YcoQTL5bA1W.EKIBeZ/RbjymsLHXod0HSjy0w.NSJHFsKu1e');
 CREATE TABLE favorites (
    username TEXT NOT NULL,
    type TEXT NOT NULL,
