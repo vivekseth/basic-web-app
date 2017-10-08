f9b5f03933d2406e3e7838b50bc82a6141aaf978
Vivek Seth <Vivekseth.m@gmail.com>
2017-10-08 10:10:49 -0700

    refactor FilmDetail.jsx

--- client/components/Pages/FilmDetail.jsx
+++ client/components/Pages/FilmDetail.jsx
@@ -5,101 +5,75 @@ import AsyncCharacterItem from '../Util/AsyncCharacterItem.jsx'
 import AsyncData from '../Util/AsyncData.jsx'
 import { Header, Table, List } from 'semantic-ui-react'
 
-class FilmDetail extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isLoading: false,
-      film: null
-    }
-  }
+const FilmDetailView = (props) => {
+  const film = props.data;
+  return (
+    <div>
 
-  _filmID() {
-    return this.props.match.params.filmID
-  }
+      <Header as='h1'>
+        {film.title}
+      </Header>
 
-  componentDidMount() {
-    this.setState({
-      isLoading: true,
-    })
+      <Header as='h2'>
+        Information
+      </Header>
 
-    superagent
-      .get('http://localhost:8080/api/films/' + this._filmID())
-      .end((err, res) => {
-        this.setState({
-          isLoading: false,
-          film: res.body
+      <Table basic='very'>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Director</Table.Cell>
+            <Table.Cell>{film.director}</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Producer</Table.Cell>
+            <Table.Cell>{film.producer}</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Release Date</Table.Cell>
+            <Table.Cell>{film.release_date}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+
+      <Header as='h2'>
+        Opening Crawl
+      </Header>
+
+      <p>
+        {film.opening_crawl}
+      </p>
+
+      <Header as='h2'>
+        Characters
+      </Header>
+
+      <List bulleted>
+      {
+        film.characters.map((url, i) => {
+          const components = url.split('/');
+          const characterID = components[components.length - 2];
+          return (
+            <List.Item key={characterID}>
+              <AsyncCharacterItem characterID={characterID} />
+            </List.Item>
+          );
         })
-      });
-  }
-
-  render() {
-    const film = this.state.film;
-
-    if (this.state.isLoading) {
-      return (<p>loading...</p>);
-    }
-    else if (!film) {
-      return (<p>There is no film with that ID</p>); 
-    }
-    else {
-      return (
-        <div>
-
-          <Header as='h1'>
-            {film.title}
-          </Header>
-
-          <Header as='h2'>
-            Information
-          </Header>
-
-          <Table basic='very'>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>Director</Table.Cell>
-                <Table.Cell>{film.director}</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Producer</Table.Cell>
-                <Table.Cell>{film.producer}</Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Release Date</Table.Cell>
-                <Table.Cell>{film.release_date}</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
-
-          <Header as='h2'>
-            Opening Crawl
-          </Header>
-
-          <p>
-            {film.opening_crawl}
-          </p>
-
-          <Header as='h2'>
-            Characters
-          </Header>
-
-          <List bulleted>
-          {
-            film.characters.map((url, i) => {
-              const components = url.split('/');
-              const characterID = components[components.length - 2];
-              return (
-                <List.Item key={characterID}>
-                  <AsyncCharacterItem characterID={characterID} />
-                </List.Item>
-              );
-            })
-          } 
-          </List>
-        </div>
-      )
-    }
-  }
+      } 
+      </List>
+    </div>
+  )
 }
 
-export default FilmDetail;
+const FilmDetailPage = (props) => {
+  const apiURL = 'http://localhost:8080/api/films/' + props.match.params.filmID;
+  return (
+    <AsyncData 
+      apiURL={apiURL}
+      IsLoading={(props) => {return (<p>Loading...</p>)}}
+      NoData={(props) => {return (<p>There is no film with that ID</p>)}}
+      HasData={FilmDetailView}
+    />
+  );
+}
+
+export default FilmDetailPage;
